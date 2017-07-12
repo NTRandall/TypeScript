6318ab157a3974686d93fdc87c41d9509e713349
Nathan Randall <nathan.randall@TCC.local>
2017-07-12 13:50:07 -0400

    Initial commit:

---
+++ 2-typescript-m2-exercise-files/Before/TypeScriptTypes/Scripts/02-04-ambient-declarations.ts
@@ -0,0 +1,13 @@
+﻿/// <reference path="typings/knockout-2.2.d.ts" />
+declare var ko: KnockoutStatic;
+module demo_02_04 {
+    var name = ko.observable("John Papa");
+    var id = ko.observable(1);
+    var guy = {
+        id: id,
+        fullName: name
+    };
+
+    var value: string = guy.fullName();
+    console.log(value);
+}
---
+++ 2-typescript-m2-exercise-files/Before/TypeScriptTypes/Scripts/dynamic-types.js
@@ -0,0 +1,12 @@
+var demo_02_03;
+(function (demo_02_03) {
+    var person;
+    person = "John Papa";
+    console.log(person);
+    person = {
+        name: "Colleen",
+        age: 25
+    };
+    console.log(person.substring(1, 4));
+})(demo_02_03 || (demo_02_03 = {}));
+//# sourceMappingURL=dynamic-types.js.map
